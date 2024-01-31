4370870d7773506d7e392dcc0ca6bfbbf40f16a7
Ilaria Rosati <llamavida@gmail.com>
2024-01-31 16:54:56 +0100

    U1-W3-D3

---
+++ assets/JS.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+const ulList = document.getElementById("firstUl");
+
+let funzione1 = function () {
+  const inputLista = document.getElementById("input1");
+  const myData = {
+    inserisciLista: inputLista.value,
+  };
+  let li = document.createElement("li");
+  li.innerText = myData.inserisciLista;
+  ulList.appendChild(li);
+  inputLista.value = "";
+  console.log(ulList);
+};
+
+const buttonClick = document.getElementById("button1");
+buttonClick.onclick = funzione1;
+
+const funzione2 = function (e) {
+  e.currentTarget.classList.add("addClass");
+};
+
+li.addEventListener("click", funzione2);
+
+console.log(ulList);
